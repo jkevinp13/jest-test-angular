ad2a2bdc8140935f0d7a631b308723874e2d2309
Kevin Peña <114270766+jkevinp13@users.noreply.github.com>
2023-10-05 19:21:49 +0000

    primeras pruebas

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,9 @@
-const path = require('path');
+const path = require("path");
 const rootDir = path.join(__dirname);
 module.exports = {
-    preset: 'jest-preset-angular',
-    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-    transform: {
-      '^.+\\.(ts|js|html)$': ['ts-jest', {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-      }]
-    },
-    testMatch: ['<rootDir>/src/**/*.spec.ts'],
-    testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-    coverageReporters: ['html', 'lcov', 'text'],
-  };
-  
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  testMatch: ["<rootDir>/src/**/*.spec.ts"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  coverageReporters: ["html", "lcov", "text"],
+};
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+})
+export class CounterComponent {}
