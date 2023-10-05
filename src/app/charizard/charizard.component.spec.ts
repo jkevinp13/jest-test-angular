85043389c9e9b758c3ffc80683b10bed9a9a9295
Kevin Peña <114270766+jkevinp13@users.noreply.github.com>
2023-10-05 22:28:49 +0000

    add unit test

--- src/app/charizard/charizard.component.spec.ts
+++ src/app/charizard/charizard.component.spec.ts
@@ -1,21 +1,98 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { CharizardComponent } from './charizard.component';
+import { PokemonService } from '../services/pokemon.service';
+import { of } from 'rxjs';
+import { Pokemon } from '../interfaces/pokemon.interface';
 
 describe('CharizardComponent', () => {
   let component: CharizardComponent;
   let fixture: ComponentFixture<CharizardComponent>;
+  let compiled: HTMLElement;
+  let service: PokemonService;
+  let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CharizardComponent]
-    });
-    fixture = TestBed.createComponent(CharizardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CharizardComponent],
+      imports: [HttpClientTestingModule],
+      providers: [PokemonService],
+    }).compileComponents();
   });
 
-  it('should create', () => {
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CharizardComponent);
+    component = fixture.componentInstance;
+    service = TestBed.inject(PokemonService);
+    httpMock = TestBed.inject(HttpTestingController);
+
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+  });
+
+  test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('debe de hacer match con el snapshot', () => {
+    expect(compiled.innerHTML).toMatchSnapshot();
+  });
+
+  test('debe de mostrar un loading al inicio', () => {
+    const h2 = compiled.querySelector('h2');
+    expect(h2?.textContent).toContain('Loading...');
+  });
+
+  test('debe de cargar a charizard inmediatamente', () => {
+    const dummyPokemon = {
+      name: 'charizardo!!',
+      sprites: {
+        front_default: 'https://charizard.com/sprite.png',
+      },
+    };
+
+    const request = httpMock.expectOne('https://pokeapi.co/api/v2/pokemon/6');
+    expect(request.request.method).toBe('GET');
+    request.flush(dummyPokemon);
+
+    fixture.detectChanges();
+    // console.log(compiled.innerHTML);
+    const h3 = compiled.querySelector('h3');
+    const img = compiled.querySelector('img');
+
+    expect(h3?.textContent?.toLocaleLowerCase()).toContain(
+      dummyPokemon.name.toLocaleLowerCase(),
+    );
+    expect(img?.src).toBe(dummyPokemon.sprites.front_default);
+    expect(img?.alt).toBe(dummyPokemon.name);
+  });
+  test('debe de cargar a charizard inmediatamente con jestSpyOn', () => {
+    const dummyPokemon = {
+      name: 'charizardo!!',
+      sprites: {
+        front_default: 'https://charizard.com/sprite.png',
+      },
+    };
+    const spyService = jest
+      .spyOn((component as any).pokemonService, 'getPokemon')
+      .mockReturnValue(of(dummyPokemon as Pokemon));
+
+    component.ngOnInit();
+
+    fixture.detectChanges();
+    expect(spyService).toHaveBeenCalledWith(6);
+    // console.log(compiled.innerHTML);
+    const h3 = compiled.querySelector('h3');
+    const img = compiled.querySelector('img');
+
+    expect(h3?.textContent?.toLocaleLowerCase()).toContain(
+      dummyPokemon.name.toLocaleLowerCase(),
+    );
+    expect(img?.src).toBe(dummyPokemon.sprites.front_default);
+    expect(img?.alt).toBe(dummyPokemon.name);
   });
 });
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -11,9 +11,9 @@ export class PokemonService {
   constructor(private http: HttpClient) {}
 
   getPokemon(id: number): Observable<Pokemon> {
-    // return this.http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    return this.http.get<Pokemon>(
-      `https://run.mocky.io/v3/e581c550-4b9b-4811-b4a3-3231720e0cdc`,
-    );
+    return this.http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    // return this.http.get<Pokemon>(
+    //   `https://run.mocky.io/v3/e581c550-4b9b-4811-b4a3-3231720e0cdc`,
+    // );
   }
 }
